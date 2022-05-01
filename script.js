d5c3aedefab4f50e87309e5488d65e887739b882
JACKE Lin Mai <102440740+JackeLinMai@users.noreply.github.com>
2022-05-01 20:31:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,109 @@
-const dino = document.querySelector('.dino');
-const background = document.querySelector('.background');
+let order = [];
+let clickedOrder = [];
+let score = 0;
 
-let isJumping = false;
-let isGameOver = false;
-let position = 0;
+// 0 - verde
+// 1 - vermelho
+// 2 - amarelo
+// 3 - azul
 
-function handleKeyUp(event) {
-    if (event.keyCode === 32) {
-        if (!isJumping) {
-            jump();
-        }
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+// cria ordem aletória de cores
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
     }
 }
 
-function jump() {
-    isJumping = true;
-
-    let upInterval = setInterval(() => {
-        if (position >= 150) {
-            // Descendo
-            clearInterval(upInterval);
-
-            let downInterval = setInterval(() => {
-                if (position <= 0) {
-                    clearInterval(downInterval);
-                    isJumping = false;
-                } else {
-                    position -= 20;
-                    dino.style.bottom = position + 'px';
-                }
-            }, 20);
-        } else {
-            // Subindo
-            position += 20;
-            dino.style.bottom = position + 'px';
-        }
-    }, 20);
+// acende a próxima cor
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
 }
 
-function createCactus() {
-    const cactus = document.createElement('div');
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
-
-    if (isGameOver) return;
-
-    cactus.classList.add('cactus');
-    background.appendChild(cactus);
-    cactus.style.left = cactusPosition + 'px';
-
-    let leftTimer = setInterval(() => {
-        if (cactusPosition < -60) {
-            // Saiu da tela
-            clearInterval(leftTimer);
-            background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-            // Game over
-            clearInterval(leftTimer);
-            isGameOver = true;
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
-        } else {
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
+// checa se os botões clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+    for (let i in clickedOrder) {
+        if (clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
         }
-    }, 20);
+    }
 
-    setTimeout(createCactus, randomTime);
+    if (clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
 }
 
-createCactus();
-document.addEventListener('keyup', handleKeyUp);
+// função para o clique do usuário
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+}
+
+// função que retorna a cor
+let createColorElement = (color) => {
+    if (color == 0) {
+        return green;
+    } else if (color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+// função para próximo nível do jogo
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+// função para game over
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+// função de início do jogo
+let playGame = () => {
+    alert('Bem vindo ao Genius! Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+// eventos de clique para as cores
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+
+// iniciando o jogo
+playGame();
